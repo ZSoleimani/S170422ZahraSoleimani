4756f878c08e5320aeb7e0420834ff2ec663cc32
Zahra <s170422@student.dtu.dk>
2020-11-07 18:32:46 +0100

    de første ændringer i MainActivity

--- app/src/main/java/com/example/s170422zahrasoleimani/MainActivity.java
+++ app/src/main/java/com/example/s170422zahrasoleimani/MainActivity.java
@@ -2,13 +2,63 @@ package com.example.s170422zahrasoleimani;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] words;
+    private Random random;
+    private String currentWord;
+    private LinearLayout wordLinearLayout;
+    private TextView[] charViews;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Resources resources=getResources();
+        words=resources.getStringArray(R.array.words);
+        random=new Random();
+        currentWord="";
+        wordLinearLayout = findViewById(R.id.word);
+        imageView=findViewById(R.id.imageView);
+
+        playGame();
+    }
+
+    private void playGame(){
+        imageView.setVisibility(View.INVISIBLE);
+        String newWord=words[random.nextInt(words.length)];
+
+        while (newWord.equals(currentWord))
+            newWord=words[random.nextInt(words.length)];
+        currentWord=newWord;
+
+        charViews =new TextView[currentWord.length()];
+        wordLinearLayout.removeAllViews();
+
+        for(int c=0;c<currentWord.length();c++){
+            charViews[c]=new TextView(this);
+            charViews[c].setText(""+currentWord.charAt(c));
+            charViews[c].setLayoutParams(new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT));
+            charViews[c].setGravity(Gravity.CENTER);
+            charViews[c].setTextColor(Color.WHITE);
+            charViews[c].setBackgroundResource(R.drawable.dash);
+            wordLinearLayout.addView(charViews[c]);
+        }
+
     }
 }
