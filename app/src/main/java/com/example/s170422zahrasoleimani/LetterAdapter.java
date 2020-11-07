f6df452cb42c647da8725edb768979609411a772
Zahra <s170422@student.dtu.dk>
2020-11-07 20:36:39 +0100

    LetterAdapter tilføjede + de første ændringer

---
+++ app/src/main/java/com/example/s170422zahrasoleimani/LetterAdapter.java
@@ -0,0 +1,48 @@
+package com.example.s170422zahrasoleimani;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+
+public class LetterAdapter extends BaseAdapter {
+
+    private String[] letters;
+    private LayoutInflater letterInflater;
+
+    public LetterAdapter(Context context){
+        letters=new String[26];
+        for(int a=0; a<letters.length; a++){
+            letters[a]=""+(char)(a+'A');
+        }
+        letterInflater=LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return letters.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @SuppressLint("ViewHolder")
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        Button letterButton;
+        letterButton= (Button) letterInflater.inflate(R.layout.letter,viewGroup,false);
+        letterButton.setText(letters[i]);
+        return letterButton;
+    }
+}
