c6e3e38d745ee310866a72be205ac87adcfb92d6
Zahra <s170422@student.dtu.dk>
2020-11-08 23:16:01 +0100

    working on win/winer

--- app/src/main/java/com/example/s170422zahrasoleimani/Win.java
+++ app/src/main/java/com/example/s170422zahrasoleimani/Win.java
@@ -4,9 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
-public class Win extends AppCompatActivity {
+public class Win extends AppCompatActivity implements View.OnClickListener {
+    TextView wordView;
 
+
+    @Override
+    public void onClick(View v) {
 
     }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
