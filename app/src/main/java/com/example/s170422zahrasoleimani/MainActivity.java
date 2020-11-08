e3376b52d5d7d9ec428de59ebab350fbd73a5aee
Zahra <s170422@student.dtu.dk>
2020-11-08 11:11:51 +0100

    StartGame færdig

--- app/src/main/java/com/example/s170422zahrasoleimani/MainActivity.java
+++ app/src/main/java/com/example/s170422zahrasoleimani/MainActivity.java
@@ -12,13 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent homeIntent = new Intent(MainActivity.this, GameActivity.class);
-                startActivity(homeIntent);
-                finish();
-            }
+        new Handler().postDelayed(() -> {
+            Intent homeIntent = new Intent(MainActivity.this, StartGame.class);
+            startActivity(homeIntent);
+            finish();
         },SPLASH_TIME_OUT);
     }
 }
--- app/src/main/java/com/example/s170422zahrasoleimani/StartGame.java
+++ app/src/main/java/com/example/s170422zahrasoleimani/StartGame.java
@@ -2,13 +2,37 @@ package com.example.s170422zahrasoleimani;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class StartGame extends AppCompatActivity {
+public class StartGame extends AppCompatActivity implements View.OnClickListener{
+
+    Button gameStart;
+    TextView playerName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_game);
+        gameStart = findViewById(R.id.button);
+        gameStart.setOnClickListener((View.OnClickListener) this);
+        playerName = findViewById(R.id.gameName);
+    }
+
+    public void onClick(View v){
+        if(v == gameStart){
+            String n = playerName.getText().toString();
+            System.out.println(n);
+            Intent intent = new Intent(this, GameActivity.class);
+            intent.putExtra("playersName", n);
+            if(n.length() > 0){
+                this.startActivity(intent);
+            }else{
+                playerName.setHint("Write Your Name");
+            }
+        }
     }
 }
