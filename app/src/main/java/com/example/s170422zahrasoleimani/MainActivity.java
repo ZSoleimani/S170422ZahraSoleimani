0559404162aae14eba92b7d41fc3d9c56a9039bb
Zahra <s170422@student.dtu.dk>
2020-11-08 10:04:29 +0100

    MainActivity changed

--- app/src/main/java/com/example/s170422zahrasoleimani/MainActivity.java
+++ app/src/main/java/com/example/s170422zahrasoleimani/MainActivity.java
@@ -1,169 +1,24 @@
 package com.example.s170422zahrasoleimani;
-
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.content.DialogInterface;
-import android.content.res.Resources;
-import android.graphics.Color;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.View;
-import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
-import java.util.Random;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
 
-    private String[] words;
-    private Random random;
-    private String currentWord;
-    private LinearLayout wordLinearLayout;
-    private TextView[] charViews;
-    private ImageView imageView;
-
-    private GridView letters;
-    private LetterAdapter letterAdapter;
-
-    private int currentPic;
-    private int nunChars;
-    private int numCorrect;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Resources resources=getResources();
-        words=resources.getStringArray(R.array.words);
-        random=new Random();
-        currentWord="";
-        wordLinearLayout = findViewById(R.id.word);
-        imageView=findViewById(R.id.imageView);
-
-        letters=findViewById(R.id.letters);
-
-        playGame();
-    }
-
-    private void playGame(){
-        imageView.setVisibility(View.INVISIBLE);
-        String newWord=words[random.nextInt(words.length)];
-
-        while (newWord.equals(currentWord))
-            newWord=words[random.nextInt(words.length)];
-        currentWord=newWord;
-
-        charViews =new TextView[currentWord.length()];
-        wordLinearLayout.removeAllViews();
-
-        for(int c=0;c<currentWord.length();c++){
-            charViews[c]=new TextView(this);
-            charViews[c].setText(""+currentWord.charAt(c));
-            charViews[c].setLayoutParams(new LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.WRAP_CONTENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT));
-            charViews[c].setGravity(Gravity.CENTER);
-            charViews[c].setTextColor(Color.WHITE);
-            charViews[c].setBackgroundResource(R.drawable.dash);
-            wordLinearLayout.addView(charViews[c]);
-        }
-
-        letterAdapter=new LetterAdapter(this);
-        letters.setAdapter(letterAdapter);
-
-        currentPic=0;
-        nunChars=currentWord.length();
-        numCorrect=0;
-
-    }
-
-    @SuppressLint("UseCompatLoadingForDrawables")
-    public void letterPressed(View view){
-        String letter=((TextView)view).getText().toString();
-        char letterChar=letter.charAt(0);
-        view.setEnabled(false);
-        view.setBackgroundResource(R.drawable.letter_down);
-        boolean correct=false;
-        for (int k=0; k<currentWord.length();k++){
-            if (currentWord.charAt(k)==letterChar){
-                correct=true;
-                numCorrect++;
-                charViews[k].setTextColor(Color.BLACK);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(homeIntent);
+                finish();
             }
-        }
-        if (correct){
-            if (numCorrect==nunChars){
-                disableButtons();
-                AlertDialog.Builder winBuild=new AlertDialog.Builder(this);
-                winBuild.setTitle("words");
-                winBuild.setMessage("You Win!\n\n The answare was:"+currentWord );
-                winBuild.setPositiveButton("Play Again",
-                        new DialogInterface.OnClickListener(){
-                    @Override
-                    public  void onClick(DialogInterface dialogInterface, int i){
-                        playGame();
-                    }
-                });
-                winBuild.setNegativeButton("Exit",
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int i) {
-                                fileList();
-
-                            }
-                        });
-                winBuild.show();
-            }
-        }
-
-        else if(currentPic<6){
-            imageView.setVisibility(View.VISIBLE);
-            switch (currentPic) {
-                case 0:
-                    imageView.setImageDrawable(getResources().getDrawable(R.drawable.p1));
-                    break;
-                case 1:
-                    imageView.setImageDrawable(getResources().getDrawable(R.drawable.p2));
-                    break;
-                case 2:
-                    imageView.setImageDrawable(getResources().getDrawable(R.drawable.p3));
-                    break;
-                case 3:
-                    imageView.setImageDrawable(getResources().getDrawable(R.drawable.p4));
-                    break;
-                case 4:
-                    imageView.setImageDrawable(getResources().getDrawable(R.drawable.p5));
-                    break;
-                case 5:
-                    imageView.setImageDrawable(getResources().getDrawable(R.drawable.p6));
-                    break;
-            }
-            currentPic++;
-        }
-        else{
-            disableButtons();
-            AlertDialog.Builder loseBuild=new AlertDialog.Builder(this);
-            loseBuild.setTitle("words");
-            loseBuild.setMessage("Game over!\n\n The answare was:"+currentWord );
-            loseBuild.setPositiveButton("Play Again",
-                    (dialogInterface, i) -> playGame());
-            loseBuild.setNegativeButton("Exit",
-                    (dialog, i) -> fileList());
-            loseBuild.show();
-
-
-        }
-
-    }
-
-    private void disableButtons() {
-        int numLetters=letters.getChildCount();
-        for (int i=0; i<numLetters; i++) {
-            letters.getChildAt(i).setEnabled(false);
-        }
+        },SPLASH_TIME_OUT);
     }
 }
