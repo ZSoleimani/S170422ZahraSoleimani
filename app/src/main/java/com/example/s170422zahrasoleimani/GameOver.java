f598298dba57fb0186e9896acd0964d0dc66eae1
Zahra <s170422@student.dtu.dk>
2020-11-08 23:04:00 +0100

    GameOver done

--- app/src/main/java/com/example/s170422zahrasoleimani/GameOver.java
+++ app/src/main/java/com/example/s170422zahrasoleimani/GameOver.java
@@ -2,13 +2,31 @@ package com.example.s170422zahrasoleimani;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class GameOver extends AppCompatActivity {
+public class GameOver extends AppCompatActivity  implements View.OnClickListener {
+
+    TextView wordView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
+
+        Intent i = getIntent();
+        String word = i.getStringExtra("word");
+
+        wordView = findViewById(R.id.theWord);
+        wordView.setText(word);
     }
+
+    @Override
+    public void onClick(View view) {
+        finish();
+
+    }
+
 }
--- app/src/main/java/com/example/s170422zahrasoleimani/Win.java
+++ app/src/main/java/com/example/s170422zahrasoleimani/Win.java
@@ -2,13 +2,11 @@ package com.example.s170422zahrasoleimani;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Win extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_win);
+
     }
-}
